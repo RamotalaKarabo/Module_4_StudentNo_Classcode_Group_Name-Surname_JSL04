6270c2ff372bdbf03698fcea60126a8385fe474a
Ramotala Karabo <KaraboJaz03@gmail.com>
2024-09-06 13:13:13 +0200

    fix bug for challenge 5 in index.js file

--- index.js
+++ index.js
@@ -37,15 +37,12 @@ console.log(`${(currentTemp <= 5) ? "Chilled to perfection" : "Needs a cooler"}`
 // otherwise, print "Better stick to water."
 
 // Define the current hour in 24-hour format
-let currentHour = 22; 
+let currentHour = 8; 
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
-
-
-
-
+console.log(`${(currentHour >= 0 && currentHour <= 7) ? "Unleash the beast" : "Better stick to water"}`);
 
 
 // In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
